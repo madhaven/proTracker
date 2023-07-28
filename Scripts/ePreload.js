270faace58a7242dfb613f929f90f11b09f5d019
madhaven <37440750+madhaven@users.noreply.github.com>
2023-07-28 18:54:11 +0530

    fix #16
    added taskClick logic
    setup better state-to-UI rendering methods
    limitted task updates to current day only

--- Scripts/ePreload.js
+++ Scripts/ePreload.js
@@ -1,16 +1,24 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('comms', {
-    newTask: (obj, callback, err) => { ipcRenderer.invoke('newTaskChannel', obj).then(callback, err) },
-    toggleTask: id => { ipcRenderer.invoke('taskClickChannel', id) },
-    loadLogs: (callback, err) => { ipcRenderer.invoke('loadLogsRequest').then(callback, err) },
-    saveData: (data, callback, err) => { ipcRenderer.invoke('saveDataRequest', data).then(callback, err) },
-    registerListener: (channel, callback) => { ipcRenderer.on(channel, callback) },
+    newTask: (obj, callback, err) => {
+        ipcRenderer.invoke('newTaskChannel', obj).then(callback, err) },
+    toggleTask: (taskId, statusId, time, callback, err) => {
+        ipcRenderer.invoke('taskClickChannel', taskId, statusId, time).then(callback, err) },
+    loadLogs: (callback, err) => {
+        ipcRenderer.invoke('loadLogsRequest').then(callback, err) },
+    saveData: (data, callback, err) => {
+        ipcRenderer.invoke('saveDataRequest', data).then(callback, err) },
+    registerListener: (channel, callback) => {
+        ipcRenderer.on(channel, callback) },
     // variables be also exposed, not just functions
 })
 
 contextBridge.exposeInMainWorld('state', {
-    notifyUIEvent: state => { ipcRenderer.send('UIEventNotifications', state) },
-    requestUIChange: (state, callback, err) => { ipcRenderer.invoke('UIEventRequests', state).then(callback, err) },
-    registerListener: (channel, callback) => { ipcRenderer.on(channel, callback) }
+    notifyUIEvent: state => {
+        ipcRenderer.send('UIEventNotifications', state) },
+    requestUIChange: (state, callback, err) => {
+        ipcRenderer.invoke('UIEventRequests', state).then(callback, err) },
+    registerListener: (channel, callback) => {
+        ipcRenderer.on(channel, callback) }
 })
