b64477623fe99e8a481ad40a1394ca6f94661f37
madhaven <37440750+madhaven@users.noreply.github.com>
2023-08-21 04:04:18 +0530

    export backend WIP

--- Scripts/Services/FileService.js
+++ Scripts/Services/FileService.js
@@ -3,13 +3,15 @@ const csv = require('fast-csv')
 const fs = require('fs')
 
 const FileService = class {
-    static options = [{ name: 'CSV Files', extensions: ['csv']}]
-    static filters = { filters: this.options }
+    static csv_options = [{ name: 'CSV Files', extensions: ['csv']}]
+    static csv_filters = { filters: this.csv_options }
+    static xl_options = [{ name: 'Excel Files', extensions: ['xlsx']}]
+    static xl_filters = { filters: this.xl_options}
 
     static loadAFile = async win => { // deprecated
         // TODO: verify if cSV is in prescribed format
         return new Promise((resolve, reject) => {
-            dialog.showOpenDialog(win, this.options)
+            dialog.showOpenDialog(win, this.csv_options)
                 .then((cancelled, filePaths) => {
                     if (cancelled)
                         resolve([false, undefined])
@@ -32,21 +34,38 @@ const FileService = class {
         })
     }
 
-    static saveFile = async (win, data) => {
+    // static saveFile = async (win, data) => {
+    //     return new Promise((resolve, reject) => {
+    //         dialog.showSaveDialog(win, this.csv_filters)
+    //             .then(({canceled, filePath}) => {
+    //                 console.table({'canceled': canceled, 'filePath': filePath})
+    //                 if (canceled) {
+    //                     resolve(false)
+    //                 } else {
+    //                     fs.writeFileSync(filePath, data)
+    //                     resolve(true)
+    //                 }
+    //             })
+    //             .catch(error => {
+    //                 console.error("FileService: fileSaveDialog", error) // TODO remove error logs
+    //                 // TODO logging
+    //                 reject(false)
+    //             })
+    //     })
+    // }
+
+    static selectFileSaveName = async win => {
         return new Promise((resolve, reject) => {
-            dialog.showSaveDialog(win, this.filters)
+            dialog.showSaveDialog(win, this.xl_filters)
                 .then(({canceled, filePath}) => {
-                    console.table({'canceled': canceled, 'filePath': filePath})
-                    if (canceled) {
+                    console.log('canceled', canceled, 'filePath', filePath)
+                    if (canceled)
                         resolve(false)
-                    } else {
-                        fs.writeFileSync(filePath, data)
-                        resolve(true)
-                    }
+                    else
+                        resolve(filePath)
                 })
                 .catch(error => {
-                    console.error("FileService: fileSaveDialog", error) // TODO remove error logs
-                    // TODO logging
+                    console.error('FileService: fileSaveDialog', error) // TODO remove error logs
                     reject(false)
                 })
         })
--- Scripts/handlers.js
+++ Scripts/handlers.js
@@ -9,6 +9,8 @@ const { ProjectProvider } = require('./Providers/ProjectProvider')
 const { TaskProvider } = require('./Providers/TaskProvider')
 const { StatusProvider } = require('./Providers/StatusProvider')
 const { StatusLogProvider } = require('./Providers/StatusLogProvider')
+const ExcelJS = require('exceljs')
+
 var tp, pp, slp, sp
 
 const loadLogsHandler = async (event) => {
@@ -22,16 +24,66 @@ const loadLogsHandler = async (event) => {
     } : false : false
 }
 
-const saveDataHandler = async (event, mainWindow) => {
+const test = async (event, string) => {
+    eval(string)
+}
+
+
+const exportDataHandler = async (event, mainWindow, logTree, tasks, projects) => {
     // prompts user for file name and saves the data into the file
+    // TODO: fetch from backend instead of front
 
-    const csvData = "date,project,task,status\n"
+    const wb = new ExcelJS.Workbook()
+        , ws = wb.addWorksheet('Logs')
+        , COLUMN_DATE = 1
+        , COLUMN_PROJECT = 2
+        , COLUMN_PENDING = 3
+        , COLUMN_DONE = 4
+        , BORDER_TOP_THIN = { top: { style: 'thin', color: 'black' }}
+        , BORDER_LEFT_THIN = { left: { style: 'thin', color: 'black' }}
+        , BORDER_BOTTOM_THICC = { bottom: { style: 'thick', color: 'black' }}
+        , FONT_RED = { color: {argb: 'FF990033' }}
+        , FONT_GREEN = { color: {argb: 'FF006600' }}
 
-    const result = await FileService.saveFile(mainWindow, csvData)
-    console.log('handler: saveFile result received:', result)
-    return result 
-        ? result 
-        : ipcMain.emit("DataError", "something went wrong, didn't save")
+    ws.getCell(1, 1).value = 'Date'
+    ws.getCell(1, 2).value = 'Project'
+    ws.getCell(1, 3).value = 'Blocker'
+    ws.getCell(1, 4).value = 'Achievemnt'
+    ws.getRow(1).border = BORDER_BOTTOM_THICC
+    // ws.row(1).freeze()
+    ws.views = [ {state: 'frozen', ySplit: 1} ]
+    
+    var currentRow = 2
+    for (const day in logTree) {
+        const [year, month, date] = day.split(',')
+        ws.getCell(currentRow, COLUMN_DATE).value = `${year}-${month}-${date}`
+        ws.getRow(currentRow).border = BORDER_TOP_THIN
+        for (const taskId in logTree[day]) {
+            const projectName = projects[tasks[taskId].projectId].name
+            const summary = tasks[taskId].summary
+            const log = logTree[day][taskId]
+            ws.getCell(currentRow, COLUMN_PROJECT).value = projectName
+
+            var column = 0, font = undefined
+            if (log.statusId == 1) {
+                column = COLUMN_PENDING
+                font = FONT_RED
+            } else if (log.statusId == 4) {
+                column = COLUMN_DONE
+                font = FONT_GREEN
+            }
+            const cell = ws.getCell(currentRow, column)
+            cell.value = summary
+            cell.font = font
+            currentRow++
+        }
+    }
+    
+    const filename = await FileService.selectFileSaveName(mainWindow)
+    if (!filename) return false
+    await wb.xlsx.writeFile(filename);
+
+    return true
 }
 
 const newTaskHandler = async (event, newTask) => {
@@ -85,7 +137,8 @@ const registerHandlers = mainWindow => {
     ipcMain.handle('taskEditChannel', editTaskHandler)
     ipcMain.handle('taskClickChannel', toggleTaskHandler)
     ipcMain.handle('loadLogsRequest', loadLogsHandler)
-    ipcMain.handle('saveDataRequest', () => { saveDataHandler(mainWindow) })
+    ipcMain.handle('exportDataRequest', (event, a, b, c) => { exportDataHandler(event, mainWindow, a, b, c) })
+    ipcMain.handle('test', test)
     
     // state info exchange
     ipcMain.on('UIEventNotifications', stateEventHandler)
