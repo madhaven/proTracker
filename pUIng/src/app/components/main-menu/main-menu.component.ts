4860f5e9d3c703da7eb4738c8d1d76d2c584d868
madhaven <37440750+madhaven@users.noreply.github.com>
2024-04-30 19:25:21 +0530

    Export

--- pUIng/src/app/components/main-menu/main-menu.component.ts
+++ pUIng/src/app/components/main-menu/main-menu.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UiStateService } from '../../services/ui-state.service';
-import { MenuTabs } from '../../common/menu-tabs';
 import { Subscription } from 'rxjs';
 import { NgIf } from '@angular/common';
 import { LoaderComponent } from '../../common/loader/loader.component';
+import { ExportButtonState } from '../../common/export-button-state';
 
 @Component({
   selector: 'pui-main-menu',
@@ -25,10 +25,8 @@ export class MainMenuComponent implements OnInit {
   loadStateObserver: Subscription;
   
   menuVisible: boolean = true;
-  logChart: MenuTabs = MenuTabs.TaskLogs;
-  habits: MenuTabs = MenuTabs.Habits;
-  projects: MenuTabs = MenuTabs.Projects;
-  export: MenuTabs = MenuTabs.Export;
+  ebs = ExportButtonState;
+  exportButtonState = ExportButtonState.Idle;
 
   idleTime: number = 0;
   idleTolerance: number = 500;
@@ -72,5 +70,55 @@ export class MainMenuComponent implements OnInit {
     this.menuVisible = visible == null
       ? !this.menuVisible
       : visible;
+  }
+
+  exportData(target: HTMLElement) {
+    const messageShowDuration = 2000;
+    this.toggleMenuBar();
+    this.exportButtonState = this.ebs.Waiting;
+
+    this.uiStateService.exportData().then(
+      (res: any) => {
+        this.exportButtonState = (res == true)
+          ? this.ebs.Success 
+          : this.ebs.Failure;
+        // TODO: standardize responses
+        // TODO: notifications
+        switch (res) {
+          case true:
+            this.exportButtonState = this.ebs.Success;
+            target.title = "Export Complete";
+            break;
+          case false:
+            this.exportButtonState = this.ebs.Failure;
+            target.title = "the previous export action was cancelled";
+            break;
+          case "noAccess":
+            this.exportButtonState = this.ebs.Failure;
+            target.title = "proTracker doesn't have access to the file at the moment";
+            break;
+          case "exportException":
+            this.exportButtonState = this.ebs.Failure;
+            target.title = "an unhandled error occurred";
+            break;
+          default:
+            this.exportButtonState = this.ebs.Idle;
+            target.title = "";
+            break;
+        }
+        setTimeout(() => {
+          this.exportButtonState = this.ebs.Idle;
+        }, messageShowDuration);
+      },
+      (err: any) => {
+        target.title = "Fatal error";
+        this.exportButtonState = this.ebs.Failure;
+        console.error('Unhandled Error occurred on Data Export', err);
+
+        setTimeout(() => {
+          this.exportButtonState = this.ebs.Idle;
+        }, messageShowDuration);
+      }
+    );
   }
 }
