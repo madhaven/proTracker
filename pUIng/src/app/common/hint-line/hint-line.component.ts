7eea6832abe6630d464bfa5823f6de2aa03e0bc6
madhaven <37440750+madhaven@users.noreply.github.com>
2024-07-02 01:50:36 +0530

    hide Hint functionality

--- pUIng/src/app/common/hint-line/hint-line.component.ts
+++ pUIng/src/app/common/hint-line/hint-line.component.ts
@@ -20,24 +20,36 @@ export class HintLineComponent {
   timer2?: NodeJS.Timeout;
   timer3?: NodeJS.Timeout;
   hintListener: Subscription;
+  hideHintListener: Subscription;
 
   constructor (private hintingService: HintingService) {
     this.hintListener = this.hintingService.hintRequested$.subscribe(
       hint => { this.showHint(hint); }
     );
+    this.hideHintListener = this.hintingService.hideHintRequested$.subscribe(
+      () => { this.hideHint(); }
+    );
   }
 
   showHint(text: string) {
-    // clear existing timers
-    if (this.timer1) { clearTimeout(this.timer1); }
-    if (this.timer2) { clearTimeout(this.timer2); }
-    if (this.timer3) { clearTimeout(this.timer3); }
-    
+    this.clearTimers();
     this.hint = text;
 
     // set timers to hide hint
     this.timer1 = setTimeout(() => { this.isVisible = true; }, 100);
     this.timer2 = setTimeout(() => { this.isVisible = false; }, 2000);
     this.timer3 = setTimeout(() => { this.hint = ""; }, 4000);
+  }
+
+  hideHint(): void {
+    this.clearTimers();
+    this.isVisible = false;
+    this.timer1 = setTimeout(() => { this.hint = ""; }, 2000);
+  }
+
+  clearTimers(): void {
+    if (this.timer1) { clearTimeout(this.timer1); }
+    if (this.timer2) { clearTimeout(this.timer2); }
+    if (this.timer3) { clearTimeout(this.timer3); }
   }
 }
--- pUIng/src/app/services/hinting.service.ts
+++ pUIng/src/app/services/hinting.service.ts
@@ -8,8 +8,14 @@ export class HintingService {
 
   private hint = new Subject<string>();
   hintRequested$ = this.hint.asObservable();
+  private hider = new Subject<void>();
+  hideHintRequested$ = this.hider.asObservable();
 
   showHint(hint: string) {
     this.hint.next(hint);
+  }
+
+  hideHint() {
+    this.hider.next();
   }
 }
