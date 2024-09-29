4022f31ca77e4ab7c5fb0cd6c1f39e9af35d6e43
madhaven <37440750+madhaven@users.noreply.github.com>
2024-09-30 04:02:15 +0530

    charts refined

--- pUIng/src/app/components/tabs-habit/habit-item/habit-item.component.ts
+++ pUIng/src/app/components/tabs-habit/habit-item/habit-item.component.ts
@@ -1,9 +1,9 @@
 import { NgIf } from '@angular/common';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EditableItemComponent } from '../../../common/editable-item/editable-item.component';
 import { Habit } from '../../../models/habit.model';
 import { UiStateService } from '../../../services/ui-state.service';
-import { Chart, registerables } from 'chart.js';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 @Component({
   selector: 'pui-habit-item',
@@ -11,6 +11,10 @@ import { Chart, registerables } from 'chart.js';
   imports: [
     EditableItemComponent,
     NgIf,
+    BaseChartDirective,
+  ],
+  providers: [
+    provideCharts(withDefaultRegisterables()) // minimize bundle with specific components
   ],
   templateUrl: './habit-item.component.html',
   styleUrl: './habit-item.component.css'
@@ -22,33 +26,22 @@ export class HabitItemComponent implements OnInit {
   today: Date = new Date();
   uiStateService: UiStateService;
   
-  chart: any;
-  chartConfig: any;
-  @ViewChild('lineChart') chartRef!: ElementRef;
+  chartData = {
+    labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    datasets: [
+      {
+        label: "Steps",
+        data: [1, 4, 3, 4, 5, 2, 7]
+      }
+    ]
+  }
 
   constructor(uiStateService: UiStateService) {
     this.uiStateService = uiStateService;
-    Chart.register(...registerables)
-    this.chartConfig = {
-      type: 'line',
-      options: {},
-      data: {
-        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-        datasets: [
-          {
-            label: "Steps",
-            data: [1, 2, 3, 4, 5, 6, 7]
-          }
-        ]
-      }
-    }
   }
 
   ngOnInit() {
     this.today = new Date();
-    setTimeout(() => {
-      this.chart = new Chart(this.chartRef.nativeElement, this.chartConfig)
-    });
   }
 
   editHabit(newName: string) {
