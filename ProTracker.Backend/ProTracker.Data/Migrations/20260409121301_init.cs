5411482feffd736a0047e0568c0dddd4cc8f98a8
madhaven <37440750+madhaven@users.noreply.github.com>
2026-04-09 17:54:22 +0530

    Update Migration

---
+++ ProTracker.Backend/ProTracker.Data/Migrations/20260409121301_init.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DateAdded = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    DateTarget = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    DateCompleted = table.Column<DateTimeOffset>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Habits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Promoters = table.Column<string>(type: "TEXT", nullable: false),
+                    Demoters = table.Column<string>(type: "TEXT", nullable: false),
+                    DateAdded = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    DateCompleted = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GoalId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    CompleteBy = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CompletedOn = table.Column<DateTimeOffset>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Goals_GoalId",
+                        column: x => x.GoalId,
+                        principalTable: "Goals",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HabitId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LogTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitLogs_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitPhases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HabitId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    RequiredTimes = table.Column<int>(type: "INTEGER", nullable: false),
+                    DaysOfWeek = table.Column<int>(type: "INTEGER", nullable: true),
+                    StartedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    EndedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitPhases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitPhases_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskStatusLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    LogTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskStatusLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskStatusLogs_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitLogs_HabitId",
+                table: "HabitLogs",
+                column: "HabitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitPhases_HabitId",
+                table: "HabitPhases",
+                column: "HabitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_GoalId",
+                table: "Tasks",
+                column: "GoalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStatusLogs_TaskId",
+                table: "TaskStatusLogs",
+                column: "TaskId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HabitLogs");
+
+            migrationBuilder.DropTable(
+                name: "HabitPhases");
+
+            migrationBuilder.DropTable(
+                name: "TaskStatusLogs");
+
+            migrationBuilder.DropTable(
+                name: "Habits");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Goals");
+        }
+    }
+}
